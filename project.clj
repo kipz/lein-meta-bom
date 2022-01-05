cc2dbb897d3503d1ab89683ed0ab0d4b1077c926
James Carnegie <me@kipz.org>
2022-01-05 10:50:11 +0000

    Version 0.1.2-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.kipz/lein-meta-bom "0.1.1"
+(defproject org.kipz/lein-meta-bom "0.1.2-SNAPSHOT"
   :description "Generate a thin jar file representing a bill of materials (BOM) for use by scanners like grype, trivy etc"
   :url "https://github.com/kipz/lein-meta-bom"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
